cd3464d65adb3de5a95cb4382e69154843b2c5dc
rodrigo <rodrigodqbrn@gmail.com>
2026-09-15 16:37:17 -0300

    "Adiciona Ex 005 e oo6"

---
+++ Ex005.py
@@ -0,0 +1,5 @@
+n = int(input('Qual número? '))
+a = n - 1
+p = n + 1
+
+print('De acordo com o Número {}, o número antecessor é: {} e o posterior é: {}.'.format(n, a, p))
---
+++ Ex006.py
@@ -0,0 +1,3 @@
+n = int(input('Qual o número? '))
+
+print ('Seu dobro é ', (n * 2),', o triplo é ', (n * 3), ' e sua raiz quadradra é ', int(n ** 0.5),'.', sep='')
